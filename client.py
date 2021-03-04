a3f491bb109b924e6935e23c56a2e7940920b1e7
Fayis Muhammed <sfayizmuhammed@gmail.com>
2021-03-04 11:38:05 +0000

    Improved Client UI Fixed Bugs

--- client.py
+++ client.py
@@ -1,27 +1,48 @@
 import socket
 import sys
 
-IP = socket.gethostbyname(socket.gethostname())
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
-
+IP = socket.gethostbyname(socket.gethostname())
 DC_CODE = "#DC#"
 
-def send_message(message):
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, PORT))
+
+    print("[*] Initializing Sockets")
+    print("[*] Connecting to Server..")
+    print(f"[*] Successfully Connected to Server\t[Server IP : {HOST}]\n")
+
+except Exception as exc:
+    print("[*] Unable to connect to server")
+    sys.exit()
+
+def send(message):
     msg = message.encode('utf-8')
     msg_data = len(msg)
     send_data = str(msg_data).encode('utf-8')
     send_data += b'' * (64 - len(send_data))
-    client.send(send_data)
-    client.send(message.encode('utf-8'))
+    if not message == DC_CODE:
+        try:
+            client.send(send_data)
+            client.send(message.encode('utf-8'))
+
+        except ConnectionResetError:
+            print("[*] Server Unavailable")
+            sys.exit()
+    else:
+        try:
+            client.send(send_data)
+            client.send(message.encode('utf-8'))
+        except ConnectionResetError:
+            pass
 
 while True:
     try:
-        msg_input = input("Input Message")
-        send_message(msg_input)
+        msg_input = input(f"{IP}> ")
+        send(msg_input)
     except KeyboardInterrupt:
-        print("\nDisconnecting From Server")
-        send_message(DC_CODE)
+        send(DC_CODE)
+        print("\n\n[*] Disconnecting From Server")
         sys.exit()
